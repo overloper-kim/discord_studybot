5c02eea74763458e250f4c185ec9f1941bbd069e
overloper <overloper0225@gmail.com>
2025-08-13 00:03:29 +0900

    study: ui 사용

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-# from discord_ui import UI
+from discord import app_commands, Interaction, ButtonStyle, ui
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN_KEY = os.environ.get("TOKEN_KEY")
@@ -11,20 +13,23 @@ TOKEN_KEY = os.environ.get("TOKEN_KEY")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
-# ui = UI(bot)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.tree.command(name="안녕")
+async def test(interaction: Interaction):
+  button = ui.Button(style=ButtonStyle.green, label="안녕하세요!")
+  view = ui.View()
+  view.add_item(button)
+  await interaction.response.send_message(view=view)
 
 @bot.event
 async def on_ready():
-  # await ui.components.send()
+  await bot.tree.sync()
   print(f'Logged in as {bot.user}')
-
-@bot.command(name="안녕")
-async def test(ctx):
-  await ctx.send(f"hello {bot.user}")
 
 @bot.event
 async def setup_hook():
   await bot.load_extension("cogs.timer")
   await bot.load_extension("cogs.user")
+
 bot.run(TOKEN_KEY)
