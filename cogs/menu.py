56a88cc9523bd8b702bf0bb2e016c1bcd61700f8
overloper <overloper0225@gmail.com>
2025-09-06 23:24:35 +0900

    feat: 타이머 기능

--- cogs/menu.py
+++ cogs/menu.py
@@ -16,9 +16,9 @@ class Menu(commands.Cog):
     """메뉴를 추천합니다. (keyword = 식사 or 디저트)"""
     input_keyword = keyword
     if (input_keyword == "식사"):
-      await interaction.response.send_message(f"🍙식사 메뉴가 랜덤으로 나옵니다!")
+      await interaction.response.send_message(f"🍙 식사 메뉴가 랜덤으로 나옵니다!")
     elif (input_keyword == "디저트"):
-      await interaction.response.send_message(f"🧁디저트 메뉴가 랜덤으로 나옵니다!")
+      await interaction.response.send_message(f"🧁 디저트 메뉴가 랜덤으로 나옵니다!")
     else:
       await interaction.response.send_message(f"잘못된 명령어 입니다.")
 
--- cogs/timer.py
+++ cogs/timer.py
@@ -1,29 +1,34 @@
 """
 timer 입력 시 60초 이상이면 분으로 표시
 """
-
 import discord
 from discord.ext import commands
+from discord import Interaction, app_commands, Member, User
 import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 class Timer(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
-  @commands.command()
-  async def timer(self, ctx, seconds: int):
+  @app_commands.command(name="타이머", description="시간을 설정해주세요. (시간 기준 초(S) 단위)")
+  async def timer(self, interaction: Interaction, seconds: int):
     """타이머를 설정합니다 (예: !timer 5)"""
     input_second = seconds
     if (seconds >= 60) :
       min = seconds / 60
       seconds = seconds % 60
-      await ctx.send(f"⏰ 타이머 시작! \n {int(min)}분 {seconds}초")
+      await interaction.response.send_message(f"⏰ 타이머 시작! \n {int(min)}분 {seconds}초")
       await asyncio.sleep(input_second)
     else:
-      await ctx.send(f"⏰ 타이머 시작! {seconds} 초")
+      await interaction.response.send_message(f"⏰ 타이머 시작! {seconds} 초")
       await asyncio.sleep(seconds)
     
-    await ctx.send(f"⏰ {ctx.author.mention}, 타이머 종료!")
+    await interaction.followup.send(f"⏰ {discord.User.name}, 타이머 종료!")
 
 async def setup(bot):
   await bot.add_cog(Timer(bot))
