cdece9b2eb795f45c23682a00b35f96b64ef277a
overloper <overloper0225@gmail.com>
2025-10-04 02:46:45 +0900

    feat: mongodb collection 확인

--- mongo_client.py
+++ mongo_client.py
@@ -6,7 +6,28 @@ load_dotenv()
 MONGO_CLI = os.environ.get("MONGO_CLI")
 
 client = MongoClient(MONGO_CLI)
+db = client["overloper_discord"]
 
-db = client['sample_mflix']
+if "users" in db.list_collection_names():
+  user_col = db["users"]
+  print(f"user_col:", user_col)
+else:
+  db.create_collection["users"]
 
-print(db)
+if "menu" in db.list_collection_names():
+  menu_col = db["menu"]
+  print(f"menu_col:", menu_col)
+else:
+  db.create_collection["menu"]
+
+if "study_time" in db.list_collection_names():
+  study_col = db["study_time"]
+  print(f"study_col:", study_col)
+else:
+  db.create_collection["study_time"]
+
+if "timer" in db.list_collection_names():
+  timer_col = db["timer"]
+  print(f"timer:", timer_col)
+else:
+  db.create_collection["timer"]
